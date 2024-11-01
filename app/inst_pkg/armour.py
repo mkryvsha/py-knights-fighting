4ea4f4c1ccab1d0919efb779d6a4e9c8931debcf
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-11-01 13:45:43 +0200

    solution

--- app/inst_pkg/armour.py
+++
@@ -1,4 +0,0 @@
-class Armour:
-    def __init__(self, part: str, protection: int) -> None:
-        self.part = part
-        self.protection = protection
--- app/inst_pkg/weapon.py
+++
@@ -1,4 +0,0 @@
-class Weapon:
-    def __init__(self, name: str, power: int) -> None:
-        self.name = name
-        self.power = power
